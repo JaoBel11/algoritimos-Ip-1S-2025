59c5ebe524abfebcb9ab3b41af16ae7c3a27c3a7
JaoBell11 <sopinhabr@outlook.com>
2025-04-11 11:48:41 -0300

    notas da turma

--- notas/main.c
+++ notas/main.c
@@ -2,56 +2,76 @@
 
 int main() {
     int n, k;
-    float mediaGeral = 0.0, maiorNota = 0.0, menorNota = 10.0;
-    int aprovados = 0;
+    float mediaGeral, maiorNota, menorNota;
+    int aprovados;
+    char desejaReiniciar;
 
-    printf("Digite o numero de alunos da turma: ");
-    scanf("%d", &n);
+    do {
+       
+        mediaGeral = 0.0;
+        maiorNota = 0.0;
+        menorNota = 10.0;
+        aprovados = 0;
 
-    float notas[n];
 
-    for (int i = 0; i < n; i++) {
-        do {
-            printf("Digite a nota do aluno %d (entre 0.0 e 10.0): ", i + 1);
-            scanf("%f", &notas[i]);
-            if (notas[i] < 0.0 || notas[i] > 10.0) {
-                printf("Nota invalida! Digite uma nota entre 0.0 e 10.0.\n");
+        printf("Digite o numero de alunos da turma: ");
+        scanf("%d", &n);
+
+      
+        float notas[n];
+
+   
+        for (int i = 0; i < n; i++) {
+            do {
+                printf("Digite a nota do aluno %d (entre 0.0 e 10.0): ", i + 1);
+                scanf("%f", &notas[i]);
+                if (notas[i] < 0.0 || notas[i] > 10.0) {
+                    printf("Nota inválida! Digite uma nota entre 0.0 e 10.0.\n");
+                }
+            } while (notas[i] < 0.0 || notas[i] > 10.0);
+        }
+        for (int i = 0; i < n; i++) {
+            mediaGeral += notas[i];
+
+            if (notas[i] > maiorNota) {
+                maiorNota = notas[i];
             }
-        } while (notas[i] < 0.0 || notas[i] > 10.0);
-    }
 
-    for (int i = 0; i < n; i++) {
-        mediaGeral += notas[i];
+            if (notas[i] < menorNota) {
+                menorNota = notas[i];
+            }
 
-        if (notas[i] > maiorNota) {
-            maiorNota = notas[i];
+            if (notas[i] >= 6.0) {
+                aprovados++;
+            }
         }
 
-        if (notas[i] < menorNota) {
-            menorNota = notas[i];
+        mediaGeral /= n;
+
+
+        printf("\nDigite a posição do aluno (1 a %d) para saber sua nota: ", n);
+        scanf("%d", &k);
+
+        if (k >= 1 && k <= n) {
+            printf("A nota do aluno na posição %d é: %.2f\n", k, notas[k-1]);
+        } else {
+            printf("Posição inválida!\n");
         }
 
-        if (notas[i] >= 6.0) {
-            aprovados++;
-        }
-    }
+        
+        printf("\nResultados:\n");
+        printf("Média geral da turma: %.2f\n", mediaGeral);
+        printf("Maior nota: %.2f\n", maiorNota);
+        printf("Menor nota: %.2f\n", menorNota);
+        printf("Quantidade de alunos aprovados: %d\n", aprovados);
 
-    mediaGeral /= n;
+        
+        printf("\nDeseja reiniciar? (S/N): ");
+        scanf(" %c", &desejaReiniciar); 
 
-    printf("\nDigite a posio do aluno (1 a %d) para saber sua nota: ", n);
-    scanf("%d", &k);
+    } while (desejaReiniciar == 'S' || desejaReiniciar == 's'); 
 
-    if (k >= 1 && k <= n) {
-        printf("A nota do aluno na posicao %d é: %.2f\n", k, notas[k-1]);
-    } else {
-        printf("Posiçao invaclida!\n");
-    }
-
-    printf("\nResultados:\n");
-    printf("Media geral da turma: %.2f\n", mediaGeral);
-    printf("Maior nota: %.2f\n", maiorNota);
-    printf("Menor nota: %.2f\n", menorNota);
-    printf("Quantidade de alunos aprovados: %d\n", aprovados);
+    printf("Programa encerrado.\n");
 
     return 0;
 }
