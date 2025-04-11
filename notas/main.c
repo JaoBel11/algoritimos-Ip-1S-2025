2d222b52b874e7033f811091620dbb1d177fa6e6
JaoBell11 <sopinhabr@outlook.com>
2025-04-11 11:44:41 -0300

    notas

--- notas/main.c
+++ notas/main.c
@@ -5,14 +5,11 @@ int main() {
     float mediaGeral = 0.0, maiorNota = 0.0, menorNota = 10.0;
     int aprovados = 0;
 
-    // Solicita o número de alunos
     printf("Digite o numero de alunos da turma: ");
     scanf("%d", &n);
 
-    // Cria o vetor para armazenar as notas
     float notas[n];
 
-    // Lê as notas dos alunos
     for (int i = 0; i < n; i++) {
         do {
             printf("Digite a nota do aluno %d (entre 0.0 e 10.0): ", i + 1);
@@ -23,7 +20,6 @@ int main() {
         } while (notas[i] < 0.0 || notas[i] > 10.0);
     }
 
-    // Calcula a média geral, a maior e a menor nota, e a quantidade de aprovados
     for (int i = 0; i < n; i++) {
         mediaGeral += notas[i];
 
@@ -42,8 +38,7 @@ int main() {
 
     mediaGeral /= n;
 
-    // Solicita a posição de um aluno e exibe a nota correspondente
-    printf("\nDigite a posicao do aluno (1 a %d) para saber sua nota: ", n);
+    printf("\nDigite a posio do aluno (1 a %d) para saber sua nota: ", n);
     scanf("%d", &k);
 
     if (k >= 1 && k <= n) {
@@ -52,7 +47,6 @@ int main() {
         printf("Posiçao invaclida!\n");
     }
 
-    // Exibe os resultados
     printf("\nResultados:\n");
     printf("Media geral da turma: %.2f\n", mediaGeral);
     printf("Maior nota: %.2f\n", maiorNota);
